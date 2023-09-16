5c823037c52b498f731f9948a6de069c4a11acab
CreatorLZ <isaacanyim3@gmail.com>
2023-09-16 08:46:52 +0100

    Update Spinner2.jsx

--- src/components/Spinner2.jsx
+++ src/components/Spinner2.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 const Container = styled.div`
     width: 100%;
-    height: 100%;
+    height: 100vh;
     display: flex;
     align-items: center;
     justify-content: center;
