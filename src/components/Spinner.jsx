afb861d82caadcdfa061a3bd72bcf9c5c3b1e1a7
CreatorLZ <isaacanyim3@gmail.com>
2023-12-17 11:10:01 -0800

    giving

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
 const Container = styled.div`
-    width: 80%;
-    height: 100vh;
+    width: 100%;
+    height: 100%;
     display: flex;
     align-items: center;
     justify-content: center;
